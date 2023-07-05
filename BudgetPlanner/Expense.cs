b59a503bb4393a4ce3e6a21fcdded3910f66914b
Dawid Stepniewski <dawidstepniewski999@gmail.com>
2023-07-05 08:32:30 +0200

    Add ToString method to Expense class

---
+++ BudgetPlanner/Expense.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner
+{
+    public class Expense
+    {
+        private decimal _amount;
+        private ExpenseCategory _category;
+        private DateTime _date;
+
+        public Expense(decimal amount, ExpenseCategory category, DateTime date)
+        {
+            this._amount = amount;
+            this._category = category;
+            this._date = date;
+        }
+
+        public void SetAmount(decimal amount)
+        {
+            _amount = amount;
+        }
+        public decimal GetAmount()
+        {
+            return _amount;
+        }
+
+        public void SetCategory(ExpenseCategory category)
+        {
+            _category = category;
+        }
+        public ExpenseCategory GettCategory()
+        {
+            return _category;
+        }
+
+        public void SetDate(DateTime date)
+        {
+            _date = date;
+        }
+        public DateTime GeDate()
+        {
+            return _date;
+        }
+
+        public override string ToString()
+        {
+            return $"Amount: {_amount}, Category: {_category}, Date: {_date}";
+        }
+
+    }
+}
