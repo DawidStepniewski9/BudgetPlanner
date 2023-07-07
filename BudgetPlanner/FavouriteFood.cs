e6422f91137773dfbc98077421309cfe53824c93
Dawid Stepniewski <dawidstepniewski999@gmail.com>
2023-07-07 14:36:07 +0200

    Add 2 classes: GroceriesExpense and FavouriteFood, both inheriting from the Expense class.
    GroceriesExpense include:
    string _nameMeat;
    string _nameFV;
    string _fruitOrVegetable;
    
    GroceriesExpense include:
    string _nameFood;
    string _nameRestaurant;

---
+++ BudgetPlanner/FavouriteFood.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner
+{
+    public class FavouriteFood : Expense 
+    {
+        private string _nameFood;
+        private string _nameRestaurant;
+
+        public FavouriteFood(string nameFood, string nameRestaurant, decimal amount, 
+                             ExpenseCategory category, DateTime date) 
+                            : base(amount, category, date)
+        {
+            this._nameFood = nameFood;
+            this._nameRestaurant = nameRestaurant;
+        }
+
+        public void SetNameFood(string nameFood)
+        {
+            _nameFood = nameFood;
+        }
+        public string GetNameFood()
+        {
+            return _nameFood;
+        }
+
+        public void SetNameRestaurant(string nameRestaurant)
+        {
+            _nameRestaurant = nameRestaurant;
+        }
+        public string GetNameRestaurant()
+        {
+            return _nameRestaurant;
+        }
+    }
+}
---
+++ BudgetPlanner/GroceriesExpense.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner
+{
+    public class GroceriesExpense : Expense
+    {
+        private string _nameMeat;
+        private string _nameFV;
+        private string _fruitOrVegetable;
+
+        public GroceriesExpense(string nameMeat, string nameFV, string fruitOrVegetable, 
+                                decimal amount, ExpenseCategory category, DateTime date)
+                                : base(amount, category, date)
+        {
+            this._nameMeat = nameMeat;
+            this._nameFV = nameFV;
+            this._fruitOrVegetable = fruitOrVegetable;
+        }
+
+        public void SetNameMeat(string nameMeat)
+        {
+            _nameMeat = nameMeat;
+        }
+        public string GetNameMeat()
+        {
+            return _nameMeat;
+        }
+
+        public void SetNameFV(string nameFV)
+        {
+            _nameFV = nameFV;
+        }
+        public string GetNameFV()
+        {
+            return _nameFV;
+        }
+
+        public void SetFruitOrVegetable(string fruitOrVegetable)
+        {
+            _fruitOrVegetable = fruitOrVegetable;
+        }
+        public string GetFruitOrVegetable()
+        {
+            return _fruitOrVegetable;
+        }
+    }
+}
